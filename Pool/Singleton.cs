c6119053b1dc3e67902c33b68c54c6464448fe8f
VinhNguyen0811 <46274831+VinhNguyen0811@users.noreply.github.com>
2018-12-31 20:44:44 +0700

    Add files via upload

---
+++ Pool/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get { return instance; }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        instance = null;
+    }
+}
